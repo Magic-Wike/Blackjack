08a1376ff190292367dace848aebe843775dd962
Your Name <withrowmd@gmail.com>
2022-03-07 00:46:54 -0500

    updating misc files...curious is workspace file works on different device

--- scratchpad.py
+++ scratchpad.py
@@ -1,4 +1,5 @@
-# ## a pile of junk, ignore
+# # ## a pile of junk, ignore
+# import random
 
 # class Deck:
 #     card_names = ['Two of Clubs', 'Two of Spades', 'Two of Diamonds', 'Two of Hearts', 'Three of Clubs', 'Three of Spades', 'Three of Diamonds', 'Three of Hearts', 'Four of Clubs', 'Four of Spades', 'Four of Diamonds', 'Four of Hearts', 'Five of Clubs', 'Five of Spades', 'Five of Diamonds', 'Five of Hearts', 'Six of Clubs', 'Six of Spades', 'Six of Diamonds', 'Six of Hearts', 'Seven of Clubs', 'Seven of Spades', 'Seven of Diamonds', 'Seven of Hearts', 'Eight of Clubs', 'Eight of Spades', 'Eight of Diamonds', 'Eight of Hearts', 'Nine of Clubs', 'Nine of Spades', 'Nine of Diamonds', 'Nine of Hearts', 'Ten of Clubs', 'Ten of Spades', 'Ten of Diamonds', 'Ten of Hearts', 'Jack of Clubs', 'Jack of Spades', 'Jack of Diamonds', 'Jack of Hearts', 'Queen of Clubs', 'Queen of Spades', 'Queen of Diamonds', 'Queen of Hearts', 'King of Clubs', 'King of Spades', 'King of Diamonds', 'King of Hearts', 'Ace of Clubs', 'Ace of Spades', 'Ace of Diamonds', 'Ace of Hearts']
@@ -9,8 +10,43 @@
 #         self.shoe_size = str(num_shoes)
 #         pass
 
-# print(Deck.card_values)
-# # print(Deck.cards)
+#     def generate_deck(self, num_shoes=1):
+#         deck = []
+#         for i in range(num_shoes):
+#             for card in self.card_names:
+#                 deck.append(card)
+#             random.shuffle(deck)
+#         return deck
 
-test = input("Test:")
-print(type(test))
+#     def deal_card(self, num_cards=1):
+#         for i in range(num_cards):
+#             random_index = random.randint(1, len(self.cards)-1)
+#             random_card = self.cards[random_index]
+#         return random_card        
+
+
+# deck = Deck()
+
+# def get_card_val(card):
+#     card_val = 0
+#     for key in deck.card_values.keys():
+#         if card.startswith(key):
+#             # if card.startswith("Ace"):
+                
+#             card_val = deck.card_values[key]
+#     return card_val
+
+# print(get_card_val("Two of Diamonds"))
+# print(get_card_val("King of Diamonds"))
+# print(get_card_val("Queen of Diamonds"))
+# print(get_card_val("Four of Diamonds"))
+# print(get_card_val("Seven of Diamonds"))
+# print(get_card_val("Ace of Diamonds"))
+ 
+
+string = "Two of Hearts"
+
+split = string.split(maxsplit=1)
+split_str = str(split[0])
+print(split_str)
+
